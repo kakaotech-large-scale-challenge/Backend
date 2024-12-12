c4295276112f11deb24c62d1d69d0734da887da1
alswp006 <alswp006@gmail.com>
2024-12-12 15:43:36 +0900

    [Update] redis clustering add server

--- config/keys.js
+++ config/keys.js
@@ -13,6 +13,8 @@ module.exports = {
     { host: process.env.REDIS_MASTER1, port: 6379 },  // master1
     { host: process.env.REDIS_MASTER2, port: 6379 },   // master2
     { host: process.env.REDIS_MASTER3, port: 6379 },   // master3
+    { host: process.env.REDIS_MASTER4, port: 6379 },   // master3
+    { host: process.env.REDIS_MASTER5, port: 6379 },   // master3
   ],
   openaiApiKey: process.env.OPENAI_API_KEY,
   vectorDbEndpoint: process.env.VECTOR_DB_ENDPOINT,
